f219edca8c5a88d20aabe95ed1cd9eb9c6e9f0df
Michael Churilov <michael.churilov@gmail.com>
2023-01-10 22:26:59 -0500

    Add prisma helpers npm scripts & some comments

--- server/api/streak-types.ts
+++ server/api/streak-types.ts
@@ -4,15 +4,18 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 const { streakType } = prisma;
 
+// Index - get a list of StreakTypes
 async function getStreakTypesApi(_: Request, res: Response) {
   res.status(200).json(await streakType.findMany());
 }
 
+// Show - get 1 specific streakType
 async function showStreakTypeApi(req: Request, res: Response) {
   const { id } = req.params;
   res.status(200).json(await streakType.findUnique({ where: { id: Number(id) } }));
 }
 
+// Create
 async function createStreakTypeApi(req: Request, res: Response): Promise<void> {
   const { name } = req.body;
   const newStreakType = await streakType.create({ data: { name } });
@@ -20,6 +23,10 @@ async function createStreakTypeApi(req: Request, res: Response): Promise<void> {
   res.status(202).json(newStreakType);
 }
 
+// @TODO Update
+// Endpoint to change just the name of a streakType
 
+// @TODO Delete
+// Endpoint to delete a streaktype
 
 export { getStreakTypesApi, createStreakTypeApi, showStreakTypeApi }
