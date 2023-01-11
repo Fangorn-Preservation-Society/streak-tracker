4611eab609527dcb33320fd50411c3b165137599
Michael Churilov <michael.churilov@gmail.com>
2023-01-10 19:52:08 -0500

    Add nodemon for dev

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,12 @@
-console.log('I run')
+import express, { Response } from "express"
+
+const app = express()
+const port = 3000
+
+app.get('/', (_, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
