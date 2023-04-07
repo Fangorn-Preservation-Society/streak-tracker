9788b89555dba70d853a3638c43ee34c73e9a6b3
Michael Churilov <michael.churilov@gmail.com>
2023-04-06 23:13:53 -0400

    Makes authed request types work

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,15 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express, { NextFunction, Response } from 'express'
 import { login, register } from './api/auth'
 import {
-  getStreakTypesApi,
-  createStreakTypeApi,
-  showStreakTypeApi,
-  deleteStreakTypeApi,
-  updateStreakTypeApi,
+    getStreakTypesApi,
+    createStreakTypeApi,
+    showStreakTypeApi,
+    deleteStreakTypeApi,
+    updateStreakTypeApi,
 } from './api/streak-types'
 
 import jwt from 'jsonwebtoken'
+import { Request } from './types/request'
 
 const privateKey = String(process.env.TOKEN_KEY)
 
@@ -16,25 +17,25 @@ const app = express()
 
 // @TODO add a test, extract to its own file
 const auth = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers['x-access-token']
+    const token = req.headers['x-access-token']
 
-  if (!token || typeof token !== 'string') {
-    return res.status(403).send('A token is required for authentication')
-  }
-
-  try {
-    const decoded = jwt.verify(token, privateKey)
-
-    if (typeof decoded === 'string') {
-      throw new Error('Invalid JWT Token')
+    if (!token || typeof token !== 'string') {
+        return res.status(403).send('A token is required for authentication')
     }
 
-    const { userId } = decoded
-    req.userId = userId
-  } catch (err) {
-    return res.status(401).send('Invalid Token')
-  }
-  return next()
+    try {
+        const decoded = jwt.verify(token, privateKey)
+
+        if (typeof decoded === 'string') {
+            throw new Error('Invalid JWT Token')
+        }
+
+        const { userId } = decoded
+        req.userId = userId
+    } catch (err) {
+        return res.status(401).send('Invalid Token')
+    }
+    return next()
 }
 
 // Register our middleware
@@ -45,12 +46,12 @@ app.post('/api/register', register)
 app.post('/api/login', login)
 
 /******************
-  * Authed Routes *
-  *****************/
+ * Authed Routes *
+ *****************/
 // Streak Routes
 app.get('/api/streak-types', auth, getStreakTypesApi)
 app.get('/api/streak-types/:id', auth, showStreakTypeApi)
-app.post("/api/streak-types", auth, createStreakTypeApi);
+app.post('/api/streak-types', auth, createStreakTypeApi)
 app.delete('/api/streak-types/:id', auth, deleteStreakTypeApi)
 app.put('/api/streak-types/:id', auth, updateStreakTypeApi)
 
